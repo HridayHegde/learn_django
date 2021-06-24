99a01557a880d904119dad917d59e7888976687d
Hriday Hegde <hridayhegde1999@gmail.com>
2021-06-25 02:31:53 +0530

    Readme changes with database integration

--- polls/models.py
+++ polls/models.py
@@ -5,3 +5,9 @@ class Users(models.Model):
     userid = models.AutoField(primary_key=True)
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.username
+    
+    def get_datalist(self):
+        return [self.userid,self.username,self.password]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
-
+from .models import Users
 def index(request):
     return HttpResponse("Hi!, Welcome to my learning project")
 # Create your views here.
+
+def listofusers(request):
+    x = Users.objects.all()
+    objects = []
+    for i in x:
+        objects.append(i.get_datalist())
+    template = loader.get_template('polls/users.html')
+    context = {
+        'userlist' : objects
+    }
+    print(objects)
+    return HttpResponse(template.render(context,request))
